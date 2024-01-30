71ed0581eb07ef7e95d4e1ba3effeb5a86d06648
Marcos Alcântara <122944586+MalcanBR@users.noreply.github.com>
2024-01-30 20:14:17 -0300

    AW01 - Criando objetos cores no js

---
+++ script.js
@@ -0,0 +1,25 @@
+const verdeCipreste = {
+    nome: 'Verde-cipreste',
+    pasta: 'imagens-verde-cipreste'
+}
+const azulInverso ={
+    nome: 'azul-inverno',
+    pasta: 'imagens'
+}
+
+const meiaNoite = {
+   nome:'Meia-noite',
+   pasta: 'imagens-meia-noite'
+}
+
+const estelar = {
+    nome: 'Estelar',
+    pasta:'imagens-estelar'
+}
+
+const rosaClaro = {
+    nome: 'Rosa-claro',
+    pasta: 'imagens-rosa-claro'
+}
+
+const opcoesCores = [verdeCipreste, azulInverso, meiaNoite, estelar, rosaClaro];
